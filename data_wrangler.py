00548586ffe99e9fa409341e73178f5c92292ae1
Alex Thieken <thieken9889@gmail.com>
2019-04-24 15:29:30 -0400

    uncommented some cuda commands and changed VGG16 to VGG16 with batch norm

--- data_wrangler.py
+++ data_wrangler.py
@@ -25,14 +25,13 @@ def data_wrangler(datadir,imsize,show=False,batch_size=10):
     return train_dl, test_dl
 
 
-
-#### Code from https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html #####
+# ### Code from https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html #####
 def imshow(inp, title=None):
     """Imshow for Tensor."""
     inp = inp.numpy().transpose((1, 2, 0)) # rearrange dimensions from (color,y,x) -> (y,x,color)
-    #mean = np.array([0.485, 0.456, 0.406])
-    #std = np.array([0.229, 0.224, 0.225])
-    #inp = std * inp + mean # undo the normalization
+    # mean = np.array([0.485, 0.456, 0.406])
+    # std = np.array([0.229, 0.224, 0.225])
+    # inp = std * inp + mean # undo the normalization
     inp = np.clip(inp, 0, 1)
     # Display image, without ticks
     plt.imshow(inp)
@@ -40,7 +39,7 @@ def imshow(inp, title=None):
     plt.yticks([])
     if title is not None:
         plt.title(title)
-    plt.show() # added this to make the plot show in pycharm
+    plt.show()  # added this to make the plot show in pycharm
 
 ###########################################################################################
 
